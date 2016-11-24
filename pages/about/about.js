5ae32c7725141eeefaad3924d7de8d7077dcfc40
paranoidq <paranoid.qian@gmail.com>
2016-11-25 01:00:46 +0800

    增加user-logo到about页面

--- pages/about/about.js
+++ pages/about/about.js
@@ -3,7 +3,18 @@ var app = getApp()
 
 Page({
     data: {
-        src: 'imgs/home.png'
+        userInfo: {}
+    },
+
+    onShow: function () {
+        var that = this
+        //调用应用实例的方法获取全局数据
+        app.getUserInfo(function(userInfo){
+            //更新数据
+            that.setData({
+                userInfo:userInfo
+            })
+        })
     }
 
 })
